83fb2391fae7e3f22f67bf640e3b637627c47d4f
NatsukiShimizu <n.shimizu023@gmail.com>
2023-10-29 12:38:22 +0900

    クラス追加

--- main.py
+++ main.py
@@ -6,3 +6,33 @@ import random
 import copy
 import time
 from curses import wrapper
+
+class Speed:
+    """ゲーム速度クラス
+    """
+    pass
+
+class Food:
+    """蛇の餌クラス
+    """
+    pass
+
+class Snake:
+    """蛇クラス
+    """
+    pass
+
+class TopWindow:
+    """トップ画面クラス
+    """
+    pass
+
+class GameWindow:
+    """ゲーム画面クラス
+    """
+    pass
+
+class SettingWindow:
+    """設定画面クラス
+    """
+    pass
